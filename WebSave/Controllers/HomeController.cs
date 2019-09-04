45fc1fab795ae01a75cad7bce67f19ef77dfde1b
Codexiaoyi <874183200@.com>
2019-09-05 00:58:33 +0800

    [FIX]修改Layout界面部分内容

--- WebSave/Controllers/HomeController.cs
+++ WebSave/Controllers/HomeController.cs
@@ -11,11 +11,6 @@ namespace WebSave.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
